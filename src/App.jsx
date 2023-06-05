8941dd18fd08bfa4c0fe14d3a4560c77e490a97c
ph_deoliveira <ph_deoliveira@hotmail.com>
2023-06-04 21:15:57 -0300

    audio playing

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,27 @@
-import { useContext } from "react";
+import { useContext,  useEffect,  useState } from "react";
 import { QuizContext } from "./context/quiz";
 
 import Welcome from "./components/Welcome";
 import Question from "./components/Question";
 import GameOver from "./components/GameOver";
 
+
 import "./App.css";
 
 function App() {
   const [quizState] = useContext(QuizContext);
-  // const [audio, setAudio] = useState(new Audio('src/assets/music.mp3'))
+  const [audio, setAudio] = useState(new Audio('src/assets/music.mp3'))
 
   // var audio = new Audio('src/assets/music.mp3');
 
-  // useEffect(()=>{
-    // audio.play();
-  // })
+  useEffect(()=>{
+    console.log("oi")
+    audio.play();
+    audio.volume = 0.4
+    audio.loop = true;
+  })
+
+
 
   return (
     <div className="App">
